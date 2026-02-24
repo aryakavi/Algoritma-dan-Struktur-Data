1e2ad31b81d0632187473840ea263c56f8db1c4d
aryakavi <aryakaviimaran2007@gmail.com>
2026-02-24 19:17:49 +0700

    Percobaan1

---
+++ MInggu 2/Mahasiswa04.java
@@ -0,0 +1,33 @@
+public class Mahasiswa04 {
+
+        String nama4;
+        String nim4;
+        String kelas4;
+        double ipk4;
+
+        void tampilkanInformasi() {
+            System.out.println("Nama: " + nama4);
+            System.out.println("NIM: " + nim4);
+            System.out.println("IPK: " + ipk4);
+            System.out.println("Kelas: " + kelas4);
+        }
+
+        void ubahKelas(String kelasBaru4){
+            kelas4 = kelasBaru4;
+        }
+        void updateIpk(double ipkBaru4){
+            ipk4 = ipkBaru4;
+        }
+
+        String nilaiKinerja(){
+            if (ipk4 >= 3.5) {
+                return "Kinerja sangat baik";
+            }else if (ipk4 >= 3.0) {
+                return "Kinera baik";
+            }else if (ipk4 >= 2.0) {
+                return "Kinerja cukup";
+            }else {
+                return "Kinerja kurang";
+            }
+        }
+    }
